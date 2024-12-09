9d5fac7698d0255a508cfde36089e11181933f33
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2024-12-09 23:32:11 +0900

    Add files via upload

--- html/menu.js
+++ html/menu.js
@@ -134,16 +134,29 @@ document.querySelector("#map-management-agency-menu").addEventListener("click", 
 });
 
 // 검색 박스에서 Enter 키를 눌렀을 때 검색 실행
-document.querySelector("#tistory-search-word").addEventListener("keydown", (event) => {
+document.querySelector("#tistory-search-word").addEventListener("keydown", async (event) => {
   if (event.key === "Enter") {
     const query = document.querySelector("#tistory-search-word").value.trim();
 
     if (query) {
-      const searchUrl = `https://marketinganywhere.tistory.com/search/${encodeURIComponent(query)}`;
-      chrome.runtime.sendMessage({ action: "openLinkTab", url: searchUrl });
+      if (query.includes(",")) {
+        const splitQueries = query.split(",").map(q => q.trim()); // Split and trim each part
+        let i = 0;
+        for (const splitQuery of splitQueries) {
+          const searchUrl = `https://marketinganywhere.tistory.com/search/${encodeURIComponent(splitQuery)}`;
+          await chrome.runtime.sendMessage({ action: "openLinkTab", url: searchUrl });
+          if (i != splitQueries.length - 1) {
+            await sleep(1000); // Wait 1 second between searches
+          }
+          i++;
+        }
+      } else {
+        const searchUrl = `https://marketinganywhere.tistory.com/search/${encodeURIComponent(query)}`;
+        await chrome.runtime.sendMessage({ action: "openLinkTab", url: searchUrl });
+      }
     } else {
       alert("검색어를 입력하세요.");
-    }    
+    }
   }
 });
 
