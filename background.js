4283bfcd4a879da4a7bd6f29f3b1825b12e7ae39
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2025-12-13 10:41:51 +0900

    Add files via upload

--- background.js
+++ background.js
@@ -9,11 +9,13 @@ import "./background-tool.js";
 // 확장 프로그램 설치 또는 업데이트 시 실행
 // 다른 곳은 브라우저를 시작하거나 확장이 리로드될 때마다 실행
 chrome.runtime.onInstalled.addListener(async (details) => {
-  await chrome.storage.local.set({ floatingIconCheckboxChecked: true });
-  await chrome.storage.local.set({ attachChecked: false });
-  await chrome.storage.local.set({ chatgptAiCheckboxChecked: true });
-
-  chrome.tabs.create({ url: "html/install/index.html" });
+  if (details.reason === "install") {
+    await chrome.storage.local.set({ floatingIconCheckboxChecked: true });
+    await chrome.storage.local.set({ attachChecked: false });
+    await chrome.storage.local.set({ chatgptAiCheckboxChecked: true });
+  
+    chrome.tabs.create({ url: "html/install/index.html" });  
+  }
 
   // 확장 프로그램이 제거될 때 특정 URL로 이동
   chrome.runtime.setUninstallURL("https://docs.google.com/forms/d/e/1FAIpQLSeK13vZbukS5N8OAdNe4j4JgotbloUYvVLA94rjYOU9d1kBIA/viewform?usp=sf_link");
